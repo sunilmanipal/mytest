566b8d3c7d5bbc059d944ddf7486ac8dea6b0164
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-06-04 16:24:52 +0530

    changes done

--- Devops_project/src/test/java/test/Test_New.java
+++ Devops_project/src/test/java/test/Test_New.java
@@ -1,8 +1,14 @@
 package test;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Test_New {
+	@BeforeTest
+	public void bf()
+	{
+		System.out.println("starting");
+	}
   @Test
   public void f() {
 	  System.out.println("It's Working");
