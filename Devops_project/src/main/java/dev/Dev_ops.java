a6a1de4e96b401810ae9546815b56366fe770382
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-06-04 12:20:57 +0530

    New_Commit

---
+++ Devops_project/src/main/java/dev/Dev_ops.java
@@ -0,0 +1,10 @@
+package dev;
+
+public class Dev_ops {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Welocme to java");
+
+	}
+
+}
---
+++ Devops_project/src/test/java/test/Test_New.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Test_New {
+  @Test
+  public void f() {
+	  System.out.println("It's Working");
+  }
+}
