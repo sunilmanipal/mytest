3e7c2dc347f66c8d7e47f1335ed4383e1363d71d
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-06-29 17:18:49 +0530

    Send the project

---
+++ sunil/src/test/java/sunil/Test123.java
@@ -0,0 +1,15 @@
+package sunil;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class Test123 {
+	@Test
+	public void tt()
+	{
+		Assert.assertEquals("sunil", "sunil");
+		System.out.println("welocme to manipal");
+	}
+
+}
