f729d0b90df41d2d31cdafd8d7217a893e645c97
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-07-10 11:18:03 +0530

    Changes done

--- TestCalc/src/test/java/com/hello/StringCalculatorTest.java
+++ TestCalc/src/test/java/com/hello/StringCalculatorTest.java
@@ -2,21 +2,28 @@ package com.hello;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
 	@Test
 	public void testAdd() {
-		StringCalculator.add("1,2");
+		StringCalculator.add("3,2");
+		System.out.println("It should start after before");
+		System.out.println("this is good");
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void testIsNumberLengthGreaterThanTwo() {
 		StringCalculator.add("1,2,3");
-		System.out.println("this is ");
+		System.out.println("It should start after Test1");
+		//System.out.println("this is ");
 	}
 
+	
+	
 	/*@Test
 	public void testIsNumberEmpty() {
 		fail("Not yet implemented");
